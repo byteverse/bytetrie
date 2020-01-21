a4f7521500f6c27d59d5e1783cbc02d16c8af571
Eric Demko <edemko@layer3com.com>
2020-01-21 13:43:04 -0500

    find longest prefix of a bytestring held in a trie

--- src/Data/Trie/Word8.hs
+++ src/Data/Trie/Word8.hs
@@ -1,5 +1,7 @@
 {-# language BangPatterns #-}
 {-# language LambdaCase #-}
+{-# language MultiWayIf #-}
+{-# language ScopedTypeVariables #-}
 {-# language TupleSections #-}
 module Data.Trie.Word8
   ( Trie
@@ -10,34 +12,34 @@ module Data.Trie.Word8
   , unionWith
   , prepend
   , lookup
+  , stripPrefix
+  , stripPrefixWithKey
   , fromList
   , toList
   ) where
 
-import Prelude hiding (Maybe(..), lookup)
-import qualified Prelude
+import Prelude hiding (lookup)
 
 import Control.Applicative ((<|>))
 import Data.Bifunctor (first)
 import Data.Bytes (Bytes)
 import Data.Foldable (foldl')
 import Data.Map.Word8 (Map)
-import Data.Maybe.Unpacked (Maybe(..))
 import Data.Word (Word8)
 
 import qualified Data.Bytes as Bytes
 import qualified Data.Map.Word8 as Map
-import qualified Data.Maybe.Unpacked as Maybe
+import qualified Data.Maybe.Unpacked as UMaybe
 
 -- the `Maybe a` is a possible endpoint; it comes before the children
 -- INVARIANT: Run ctor never has Bytes length < 2
 data Trie a
   -- TODO could save four machine words with a separate Nil case
-  = Branch {-# unpack #-} !(Maybe a) !(Map (Trie a))
+  = Branch {-# unpack #-} !(UMaybe.Maybe a) !(Map (Trie a))
   -- TODO use Bytes? or ByteArray directly?
   -- ByteArray uses more copying on insert, but lookup may be faster
   -- TODO it seems like I should be able to inline the Trie, but would that help?
-  | Run {-# unpack #-} !(Maybe a) {-# unpack #-} !Bytes !(Trie a)
+  | Run {-# unpack #-} !(UMaybe.Maybe a) {-# unpack #-} !Bytes !(Trie a)
   deriving (Eq{- TODO needs instance for Map, Functor-})
 
 instance Semigroup a => Semigroup (Trie a) where (<>) = append
@@ -48,18 +50,18 @@ instance Show a => Show (Trie a) where show = show . toList
 ------------ Construction ------------
 
 empty :: Trie a
-empty = Branch Nothing Map.empty
+empty = Branch UMaybe.Nothing Map.empty
 
 singleton :: Bytes -> a -> Trie a
-singleton k v = prepend k $ Branch (Just v) Map.empty
+singleton k v = prepend k $ Branch (UMaybe.Just v) Map.empty
 
 -- Use this internally instead of the Run ctor.
 -- This ensures the run length >= 2 invariant is maintained.
 prepend :: Bytes -> Trie a -> Trie a
 prepend bytes next = case Bytes.length bytes of
   0 -> next
-  1 -> Branch Nothing (Map.singleton (Bytes.unsafeIndex bytes 0) next)
-  _ -> Run Nothing bytes next
+  1 -> Branch UMaybe.Nothing (Map.singleton (Bytes.unsafeIndex bytes 0) next)
+  _ -> Run UMaybe.Nothing bytes next
 
 insert :: Bytes -> a -> Trie a -> Trie a
 insert k v' = unionWith const (singleton k v')
@@ -97,8 +99,8 @@ unionWith f trieA trieB = case (trieA, trieB) of
     uncommon bytes = Bytes.unsafeDrop (Bytes.length common) bytes
   where
     mergeChildren left right = Map.unionWith (unionWith f) left right
-    mergeValue Nothing Nothing = Nothing
-    mergeValue (Just x) (Just y) = Just (f x y)
+    mergeValue UMaybe.Nothing UMaybe.Nothing = UMaybe.Nothing
+    mergeValue (UMaybe.Just x) (UMaybe.Just y) = UMaybe.Just (f x y)
     mergeValue x y = x <|> y
 
 
@@ -114,27 +116,55 @@ toList = \case
     f k v acc = prependList (Bytes.singleton k) (toList v) ++ acc
   Run valO run next -> fromValue valO ++ prependList run (toList next)
   where
-  fromValue valO = (mempty,) <$> Maybe.maybeToList valO
+  fromValue valO = (mempty,) <$> UMaybe.maybeToList valO
   prependList run list = first (run <>) <$> list
 
 
 ------------ Query ------------
 
-lookup :: Bytes -> Trie a -> Prelude.Maybe a
+lookup :: Bytes -> Trie a -> Maybe a
 lookup k (Branch valO children) = case Bytes.uncons k of
-  Prelude.Nothing -> Maybe.toBaseMaybe valO
+  Prelude.Nothing -> UMaybe.toBaseMaybe valO
   Prelude.Just (c, k') -> lookup k' =<< Map.lookup c children
 lookup k (Run v run next)
-  | Bytes.null k = Maybe.toBaseMaybe v
+  | Bytes.null k = UMaybe.toBaseMaybe v
   | run `Bytes.isPrefixOf` k =
     let k' = Bytes.unsafeDrop (Bytes.length run) k
     in lookup k' next
   | otherwise = Prelude.Nothing
 
+stripPrefix :: Trie a -> Bytes -> Maybe (a, Bytes)
+stripPrefix trie inp = first snd <$> stripPrefixWithKey trie inp
+
+stripPrefixWithKey :: forall a. Trie a -> Bytes -> Maybe ((Bytes, a), Bytes)
+stripPrefixWithKey trie0 rawInp = go 0 Nothing trie0
+  where
+  go :: Int -> Maybe (Bytes, a) -> Trie a -> Maybe ((Bytes, a), Bytes)
+  go into prior trie =
+    let inp = Bytes.unsafeDrop into rawInp
+        candidate = (Bytes.unsafeTake into rawInp,) <$> extractValue trie
+        found = candidate <|> prior
+    in if | Bytes.null inp -> mkReturn <$> found
+          | Run _ run next <- trie
+          , run `Bytes.isPrefixOf` inp
+            -> go (into + Bytes.length run) found next
+          | Branch _ children <- trie
+          , Just (c, _) <- Bytes.uncons inp
+          , Just next <- Map.lookup c children
+            -> go (into + 1) found next
+          | otherwise -> mkReturn <$> found
+  mkReturn (prefix, v) =
+    let post = Bytes.unsafeDrop (Bytes.length prefix) rawInp
+    in ((prefix, v), post)
+  extractValue :: Trie a -> Maybe a
+  extractValue = \case
+    Run v _ _ -> UMaybe.toBaseMaybe v
+    Branch v _ -> UMaybe.toBaseMaybe v
+
 
 ------ Helpers ------
 
-unsafeUnconsRun :: Trie a -> (Maybe a, Word8, Trie a)
+unsafeUnconsRun :: Trie a -> (UMaybe.Maybe a, Word8, Trie a)
 unsafeUnconsRun (Run v0 bs next) = (v0, c, run')
   where
   c = Bytes.unsafeIndex bs 0
--- test/Main.hs
+++ test/Main.hs
@@ -1,17 +1,20 @@
+{-# language MultiWayIf #-}
 {-# language ScopedTypeVariables #-}
 
 {-# OPTIONS_GHC -fno-warn-orphans #-}
 
 import Data.Bytes.Types (Bytes(Bytes))
-import Data.Monoid (Sum, First)
+import Data.Monoid (Sum)
 import Data.Proxy (Proxy(..))
 import Data.Trie.Word8 (Trie)
 import Data.Word (Word8)
 import Test.Tasty (defaultMain,testGroup,TestTree)
-import Test.Tasty.QuickCheck ((===),testProperty,property,Gen)
-import Test.Tasty.QuickCheck (Arbitrary,choose,vectorOf,forAll)
+import Test.Tasty.QuickCheck ((===),testProperty,property)
+import Test.Tasty.QuickCheck (Arbitrary)
+import Test.Tasty.QuickCheck (Discard(Discard))
 
 import qualified Data.Trie.Word8 as Trie
+import qualified Data.Bytes as Bytes
 import qualified GHC.Exts as Exts
 import qualified Test.QuickCheck.Classes as QCC
 import qualified Test.Tasty.QuickCheck as TQC
@@ -21,10 +24,30 @@ main = defaultMain tests
 
 tests :: TestTree
 tests = testGroup "bytetrie"
-  [ testGroup "append"
+  [ testGroup "unionWith"
     [ lawsToTest (QCC.semigroupLaws (Proxy :: Proxy (Trie [Integer])))
     , lawsToTest (QCC.monoidLaws (Proxy :: Proxy (Trie [Integer])))
     , lawsToTest (QCC.commutativeMonoidLaws (Proxy :: Proxy (Trie (Sum Integer))))
+    ]
+  , testGroup "stripPrefix"
+    [ testProperty "finds longest prefix" $
+      \(a :: Bytes) (b :: Bytes) (c :: Bytes) (d :: Bytes) ->
+        if | Just (wc,_) <- Bytes.uncons c
+           , Just (wd,_) <- Bytes.uncons d
+           , wc /= wd ->
+              let t :: Trie Int = Trie.fromList [(a, 1), (a <> b, 2), (a <> b <> d, 3)]
+                  expected = Just ((a <> b, 2), c)
+                  found = Trie.stripPrefixWithKey t (a <> b <> c)
+              in found === expected
+           | otherwise -> property Discard
+    , testProperty "finds nothing" $
+      \(a :: Bytes) (b :: Bytes) ->
+        if | Just (wa,_) <- Bytes.uncons a
+           , Just (wb,_) <- Bytes.uncons b
+           , wa /= wb ->
+              let t :: Trie Int = Trie.fromList [(a, 1)]
+              in Trie.stripPrefixWithKey t b === Nothing
+           | otherwise -> property Discard
     ]
   ]
 
